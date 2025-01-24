6796fdd4ef72350f816bca6c36d9b5037e7eadd7
tyange <usun16@gmail.com>
2025-01-24 23:27:01 +0900

    prettier & 임시 nav 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Navigation from "@/components/navigation";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navigation />
         {children}
       </body>
     </html>
---
+++ src/components/navigation.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <nav className="items-cente sticky top-0 flex justify-between bg-white p-4 shadow-md">
+      <div>임시 NAV</div>
+      <div>
+        <ul className="flex items-center gap-10">
+          <li>
+            <Link href="/">메인</Link>
+          </li>
+          <li>
+            <Link href="/shipping-schedule">운송 일정</Link>
+          </li>
+          <li>
+            <Link href="/search">검색</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
