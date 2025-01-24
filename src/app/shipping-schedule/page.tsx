cfd2f3389d6b08f628976d037d6f367b42dae1e5
tyange <usun16@gmail.com>
2025-01-24 23:20:10 +0900

    await을 붙여야만 한다니

--- src/app/shipping-schedule/page.tsx
+++ src/app/shipping-schedule/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import { SearchParams } from "next/dist/server/request/search-params";
 
 import ScheduleList from "@/components/schedule-list";
 import Filter from "@/components/filter/filter";
@@ -6,18 +7,15 @@ import Filter from "@/components/filter/filter";
 export default async function Home({
   searchParams,
 }: {
-  searchParams: {
-    page?: string;
-    pageSize?: string;
-    vesselTypes?: string;
-    shippingLines?: string;
-  };
+  searchParams: SearchParams;
 }) {
+  const params = await searchParams;
+
   return (
     <div className="w-screen h-screen">
-      <Filter searchParams={searchParams} />
+      <Filter searchParams={params} />
       <Suspense fallback={<p>Loading...</p>}>
-        <ScheduleList searchParams={searchParams} />
+        <ScheduleList searchParams={params} />
       </Suspense>
     </div>
   );
