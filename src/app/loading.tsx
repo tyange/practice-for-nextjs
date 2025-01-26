458d6dfe003913d0a7de1c8d6e475d372417f867
tyange <usun16@gmail.com>
2025-01-26 15:22:59 +0900

    loading indicator with lucide

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,9 @@
+import { LoaderCircle } from "lucide-react";
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <LoaderCircle className="animate-spin" size={48} />
+    </div>
+  );
 }
