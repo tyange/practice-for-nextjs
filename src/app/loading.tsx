0d12b527bb87546d6c83cf45760a76dd10f53c00
tyange <usun16@gmail.com>
2025-01-27 04:39:06 +0900

    loader 스타일링 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { LoaderCircle } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <LoaderCircle className="animate-spin" size={48} />
     </div>
   );
