10d0e30bea5b419e8a205088bad44c569789b9ef
tyange <usun16@gmail.com>
2025-01-27 22:58:35 +0900

    next-intl 셋업

--- src/components/dear-customer.tsx
+++ src/components/dear-customer.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 import { ChevronRight } from "lucide-react";
 
 export default function DearCustomer() {
+  const t = useTranslations("MainPage");
+
   return (
     <div className="flex items-center justify-between p-10">
       <div className="gpa-3 flex flex-col gap-3">
-        <span className="text-3xl font-bold">Dear customers,</span>
-        <span className="text-xl">sign in for special offers</span>
+        <span className="text-3xl font-bold">{t("dearCustomers")}</span>
+        <span className="text-xl">{t("signInForSpecialOffers")}</span>
       </div>
       <Link
         href="/joinfree/login"
         className="flex items-center gap-2 text-[#00ab68]"
       >
-        <span className="text-3xl font-bold">Sign in</span>
+        <span className="text-3xl font-bold">{t("signIn")}</span>
         <ChevronRight size={32} />
       </Link>
     </div>
