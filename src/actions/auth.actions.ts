a672e6b7f0098957e40b8aea0cf8d900075d0424
tyange <usun16@gmail.com>
2025-01-30 20:00:55 +0900

    with mocking api login

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -8,7 +8,7 @@ export async function login(_: any, formData: FormData) {
   const username = formData.get("username") as string;
   const password = formData.get("password") as string;
 
-  const res = await fetch("http://localhost:3001/login", {
+  const res = await fetch("http://localhost:3002/api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
@@ -28,9 +28,20 @@ export async function login(_: any, formData: FormData) {
     }
   }
 
+  // 현재는 Mocking API에서 가짜 토큰을 발급해주기 때문에, 그 토큰을 쿠키에 저장해 로그인 여부를 제어한다.
   const data = await res.json();
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  const cookieStore = await cookies();
+  cookieStore.set("token", data.token, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
 
-  createSession(data.userId);
+  // 직접 토큰을 만들어서 쿠키에 넣을 수도 있다.
+  // createSession(data.userId);
   redirect("/my-wini");
 }
 
